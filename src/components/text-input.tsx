222008ab6c0f749e69572e52878cfb1b42c180bd
Murilo Augusto <muriloagn10@gmail.com>
2026-04-27 06:56:29 -0300

    refactor: enhance TextInput component with label prop and structure adjustments

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -3,32 +3,43 @@ import { cn } from "@/utils/cn";
 
 interface TextInputProps extends React.ComponentProps<"input"> {
   icon?: Icon;
+  label?: string;
 }
 
 export function TextInput({
   id,
   className,
+  type = "text",
   icon: Icon,
+  label,
   ...props
 }: TextInputProps) {
   const textInputId = id || "text-input";
 
   return (
-    <label
-      htmlFor={textInputId}
-      className="group flex cursor-text items-center gap-2 overflow-hidden rounded-lg border border-gray-500 p-3 transition-colors focus-within:border-yellow"
-    >
-      {Icon && <Icon className="size-5 text-yellow" />}
+    <div className="flex flex-col gap-2">
+      {label && (
+        <label htmlFor={textInputId} className="font-bold text-gray-200">
+          {label}
+        </label>
+      )}
 
-      <input
-        {...props}
-        id={textInputId}
-        type="text"
-        className={cn(
-          "w-full text-base text-gray-200 outline-none placeholder:text-gray-400",
-          className,
-        )}
-      />
-    </label>
+      <label
+        htmlFor={textInputId}
+        className="group flex cursor-text items-center gap-2 overflow-hidden rounded-lg border border-gray-500 p-3 transition-colors focus-within:border-yellow"
+      >
+        {Icon && <Icon className="size-5 shrink-0 text-yellow" />}
+
+        <input
+          {...props}
+          id={textInputId}
+          type={type}
+          className={cn(
+            "w-full text-base text-gray-200 outline-none placeholder:text-gray-400",
+            className,
+          )}
+        />
+      </label>
+    </div>
   );
 }
